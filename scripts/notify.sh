79087721fcdd62c25f77f0d8531404e4c3909297
cherya <ccherya@gmail.com>
2021-11-15 09:34:35 +0300

    Delete scripts directory

--- scripts/notify.sh
+++
@@ -1,4 +0,0 @@
-msg="$*"
-curl -X POST -H 'Content-Type: application/json' \
- -d '{"chat_id": "29462028", "text": "'"$msg"'", "disable_notification": true, "parse_mode":"markdown"}' \
- https://api.telegram.org/bot"$DEPLOYMENT_BOT_TOKEN"/sendMessage
